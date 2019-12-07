d303170bbcc8627aaaba44b349b2a68d97c1ccb0
Mobin Rahman <mobin2501@gmail.com>
2019-12-07 22:49:04 +0600

    1

---
+++ code003.asm
@@ -0,0 +1,35 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+MSG1 DW 'ENTER A LOWERCASE LETTER: $'
+MSG2 DW 'IN UPPERCASE: $'  
+CHAR DB ?
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+     
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H  
+    
+    MOV AH,1 
+    INT 21H    
+    SUB AL,20H
+    MOV CHAR,AL
+    
+    MOV AH,2
+    MOV DX,0DH
+    INT 21H
+    MOV DX,0AH
+    INT 21H
+             
+    LEA DX,MSG2 
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,2
+    MOV DL,CHAR
+    INT 21H
+    
+    MAIN ENDP
